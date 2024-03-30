0d77ec1c059b89700ee53a291cff180a9131bee9
tmiranda2000 <139283665+tmiranda2000@users.noreply.github.com>
2024-03-30 09:08:27 -0400

    Add files via upload

---
+++ recipeTracking/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./App.css";
+import RecipeCreate from "./RecipeCreate";
+import RecipeList from "./RecipeList";
+import RecipeData from "./RecipeData"
+import RecipeHeader from "./RecipeHeader";
+
+function App() {
+  const [recipes, setRecipes] = useState(RecipeData);
+
+  // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
+  const addRecipe = (recipeToAdd) => setRecipes([...recipes, recipeToAdd]);
+ 
+  // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
+  const deleteRecipe = (recipeToDelete) => {
+    setRecipes((currentRecipes) => currentRecipes.filter((recipe, index) => index !== recipeToDelete))
+  }
+  
+   return (
+    <div className="App">
+      <RecipeHeader/>
+      <RecipeList recipes={recipes} deleteRecipe = {deleteRecipe}/>
+      <RecipeCreate addRecipe = {addRecipe}/>
+    </div>
+  );
+}
+
+export default App;
